d11520625f63e17c5541e3a83db372058cc94992
Sergey <progenitor123@gmail.com>
2017-07-09 14:45:37 +0700

    Add reading from server and writing to server. Unknown problems: after closing server or client socket still open.

--- src/ru/denko/Main.java
+++ src/ru/denko/Main.java
@@ -1,8 +1,18 @@
 package ru.denko;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        // write your code here
+//        EventQueue.invokeLater(() -> {
+//            ChatFrame chatFrame = new ChatFrame();
+//            chatFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//            chatFrame.setVisible(true);
+//        });
+
+        MySocket mySocket = new MySocket("localhost", 18989, "Serg");
+        mySocket.runSocket();
     }
 }
---
+++ src/ru/denko/MySocket.java
@@ -0,0 +1,64 @@
+package ru.denko;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MySocket {
+
+    private final String host;
+    private final int port;
+    private final String clientName;
+    private Logger logger = Logger.getLogger("MySocketLogger");
+
+    public MySocket(String host, int port, String clientName) {
+        this.host = host;
+        this.port = port;
+        this.clientName = clientName;
+    }
+
+    public void runSocket() throws IOException {
+        Socket socket = new Socket(InetAddress.getByName(host), port);
+        logger.log(Level.INFO, "Set socket: host: " + host + " port: "
+                + port + " name: " + clientName);
+        //socket.setSoTimeout(5000); //timeout 5 sec
+        InputStream in = socket.getInputStream();
+        OutputStream out = socket.getOutputStream();
+
+        Thread readServer = new Thread(() -> {
+            while (true) {
+                try {
+                    byte[] readBuf = new byte[1024];
+                    in.read(readBuf);
+                    System.out.println("Server: " + new String(readBuf));
+                } catch (IOException e) {
+                    System.out.println("Exception on Thread readServer");
+                    e.printStackTrace();
+                }
+            }
+        });
+        readServer.start();
+
+        Thread readConsole = new Thread(() -> {
+            byte[] buf = new byte[1024];
+            char[] consoleBuf = new char[1024];
+            //InputStream console = new BufferedInputStream(System.in);
+            while (true) {
+                try {
+                    Reader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                    bufferedReader.read(consoleBuf);
+                    out.write(new String(consoleBuf).getBytes());
+                } catch (IOException e) {
+                    System.out.println("Exception in Thread readConsole");
+                }
+            }
+        });
+        readConsole.start();
+
+
+        logger.log(Level.INFO, "client closed");
+    }
+
+}
