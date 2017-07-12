c5c1caad49a7e41a55a7617ed9522a46921be2f2
Sergey Rudenko <Naraks@yandex.ru>
2017-07-12 13:21:30 +0700

    Fixed bugs

--- src/ru/denko/Main.java
+++ src/ru/denko/Main.java
@@ -5,12 +5,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // write your code here
-//        EventQueue.invokeLater(() -> {
-//            ChatFrame chatFrame = new ChatFrame();
-//            chatFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-//            chatFrame.setVisible(true);
-//        });
 
         MySocket mySocket = new MySocket("localhost", 18989, "Serg");
         mySocket.runSocket();
--- src/ru/denko/MySocket.java
+++ src/ru/denko/MySocket.java
@@ -1,79 +1,111 @@
 package ru.denko;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import com.sun.istack.internal.NotNull;
+
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class MySocket {
+class MySocket {
 
-    private final String host;
-    private final int port;
     private final String clientName;
-    private int closed;
+    private Socket socket; //TODO подумать над final
+
     private Logger logger = Logger.getLogger("MySocketLogger");
 
-    public MySocket(String host, int port, String clientName) {
-        this.host = host;
-        this.port = port;
+    MySocket(String host, int port, String clientName) {
         this.clientName = clientName;
+        try {
+            this.socket = new Socket(InetAddress.getByName(host), port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        logger.log(Level.INFO, "Set socket: host: " + host + " port: " + port + " name: " + clientName);
     }
 
-    public void runSocket() throws IOException {
-        Socket socket = new Socket(InetAddress.getByName(host), port);
-        logger.log(Level.INFO, "Set socket: host: " + host + " port: "
-                + port + " name: " + clientName);
-        //socket.setSoTimeout(5000); //timeout 5 sec
-        InputStream in = socket.getInputStream();
-        OutputStream out = socket.getOutputStream();
-
-        out.write(clientName.getBytes());
+    void runSocket()  {
+        try {
+//            DataOutputStream d = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+//            d.writeUTF(clientName);
+//            d.flush();
+            writeToServer(clientName); //при создании клиента отправляем серверу имя клиента
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         logger.log(Level.INFO, "Sending client name to server");
 
-        Thread readServer = new Thread(() -> {
-            while (true) {
-                try {
-                    byte[] readBuf = new byte[1024];
-                    closed = in.read(readBuf);
-                    if (closed == -1) {
-                        break;
-                    }
-                    System.out.println("Server: " + new String(readBuf));
-                } catch (IOException e) {
-                    System.out.println("Exception on Thread readServer");
-                    e.printStackTrace();
-                }
-            }
-        });
-        readServer.start();
-
-        Thread readConsole = new Thread(() -> {
-//            byte[] buf = new byte[1024];
-            //InputStream console = new BufferedInputStream(System.in);
-            while (true) {
-                if (closed == -1) {
-                    break;
-                }
-                try {
-                    byte[] consoleBuf = new byte[1024];
-                    byte[] to = new byte[128];
-                    InputStream bufferedReader = new BufferedInputStream(System.in);
-                    bufferedReader.read(to);
-                    out.write(to);
-                    bufferedReader.read(consoleBuf);
-                    out.write(consoleBuf);
-                } catch (IOException e) {
-                    System.out.println("Exception in Thread readConsole");
-                }
-            }
-        });
-        readConsole.start();
-
+        readServer();
+        readConsole();
         //logger.log(Level.INFO, "client closed");
     }
 
+    private void readServer() {
+        new Thread(() -> {
+            logger.log(Level.INFO, "Thread readServer created");
+            String msg;
+            while (true) {
+                try {
+                    msg = readFrom(socket.getInputStream());
+                    System.out.println("Server: " + msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    try {
+                        socket.close();
+                    } catch (IOException e1) {
+                        System.out.println("Could not close socket");
+                    }
+                    break;
+                }
+            }
+        }).start();
+    }
+
+    private void readConsole() {
+       new Thread(() -> {
+            logger.log(Level.INFO, "Thread readConsole created");
+            String nameTo;
+            String msg;
+            while (true) {
+                try {
+                    System.out.println("Напишите имя адресата:");
+                    nameTo = readFromConsole(); //читаем адресата сообщения
+                    writeToServer(nameTo); //пишем серверу имя адресата
+                    System.out.println("Напишите сообщение:");
+                    msg = readFromConsole(); //читаем сообщение адресату
+                    writeToServer(msg); //пишем сообщение
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    try {
+                        socket.close();
+                    } catch (IOException e1) {
+                        System.out.println("Could not close socket");
+                    }
+                    break;
+                }
+            }
+        }).start();
+    }
+
+    private void writeToServer(String msg) throws IOException {
+        logger.log(Level.INFO, "Sending message to server: " + msg);
+        DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+        out.writeUTF(msg);
+        out.flush();
+    }
+
+    private String readFrom(InputStream inputStream) throws IOException {
+        DataInputStream in = new DataInputStream(new BufferedInputStream(inputStream));
+        return in.readUTF();
+    }
+
+    private String readFromConsole() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
 }
+
+
